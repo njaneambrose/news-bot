547ffc50d072b710e8093a0814084bb6ccebe23f
Ambrose Njane <njaneambrose@gmail.com>
2022-01-23 22:47:27 +0300

    reduce_size

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.send('Welcome to the world of Bots!!!');
 });
 
 module.exports = router;
